8cf9f4b8a0d8a8138d6fba692a0e7773a39838f3
SondosYassin1 <sondosalaa687@gmail.com>
2026-08-16 12:16:54 +0300

    Remove unused CityAreaCamp filter property

--- ViewModels/PropertyFilterViewModel.cs
+++ ViewModels/PropertyFilterViewModel.cs
@@ -5,7 +5,6 @@ namespace GazaRealEstatePortal.ViewModels;
 public class PropertyFilterViewModel
 {
     public string? Governorate { get; set; }
-    public string? CityAreaCamp { get; set; }
     public decimal? PriceMin { get; set; }
     public decimal? PriceMax { get; set; }
     public OperationType? OperationType { get; set; }
